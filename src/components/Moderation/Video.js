9551d9f62b5c1f10f56783c7dae97f5da0909145
bndnt <67003594+bndnt@users.noreply.github.com>
2024-09-12 07:27:23 -0400

    neeews

--- src/components/Moderation/Video.js
+++ src/components/Moderation/Video.js
@@ -1,61 +1,113 @@
-import React, {useCallback, useEffect, useMemo, useState} from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import Plyr from "plyr-react";
 import useAxiosPrivate from "../../hooks/useAxiosPrivate.js";
 
-export default function Video({video}) {
-    const privateAxios = useAxiosPrivate()
+export default function Video({ video }) {
+  const privateAxios = useAxiosPrivate();
 
-    function acceptVideo(videoId) {
-        try {
-            privateAxios.post('content/update-video-moderation/status', {
-                allowed: true,
-                videoId: videoId
-            })
-        } catch (e) {
-            console.log(e)
-        }
+  function acceptVideo(videoId) {
+    try {
+      privateAxios.post("content/update-video-moderation/status", {
+        allowed: true,
+        videoId: videoId,
+      });
+    } catch (e) {
+      console.log(e);
     }
+  }
 
-    function declineVideo(videoId) {
-        try {
-            privateAxios.post('content/update-video-moderation/status', {
-                allowed: false,
-                videoId: videoId
-            })
-        } catch (e) {
-            console.log(e)
-        }
+  function declineVideo(videoId) {
+    try {
+      privateAxios.post("content/update-video-moderation/status", {
+        allowed: false,
+        videoId: videoId,
+      });
+    } catch (e) {
+      console.log(e);
     }
+  }
 
-    const renderVideo = useMemo(() => (
-        <Plyr
-            style={{height: '440px'}}
-            clickToPlay={true}
-            options={{controls: ['progress', 'play-large', 'play', 'current-time', 'volume', 'mute', 'fullscreen']}}
-            source={{
-                type: 'video', title: 'Video', sources: [
-                    {
-                        src: video?.link,
-                        size: 720
-                    }
-                ]
-            }}/>
-    ), [video])
+  const renderVideo = useMemo(
+    () => (
+      <Plyr
+        style={{ height: "440px" }}
+        clickToPlay={true}
+        options={{
+          controls: [
+            "progress",
+            "play-large",
+            "play",
+            "current-time",
+            "volume",
+            "mute",
+            "fullscreen",
+          ],
+        }}
+        source={{
+          type: "video",
+          title: "Video",
+          sources: [
+            {
+              src: video?.link,
+              size: 720,
+            },
+          ],
+        }}
+      />
+    ),
+    [video]
+  );
 
-    return (
-        <article style={{border: 'solid green', marginBottom: 10, width: '277px'}}>
-            <img src={video?.users?.image} style={{width: '100%'}} alt='Нету фото' />
-            <p>Имя: {video?.users?.name + ' ' + video?.users?.name}</p>
-            <p>Возраст: {video?.users?.age}</p>
-            <p>Номер телефона: {video?.users?.phone_number}</p>
-            <p>Адрес электронной почты: {video?.users?.email}</p>
-            <p>Город: {video?.users?.city}</p>
-            <p>Ссылка на соцсеть: {video?.users?.social_media_link}</p>
-            <p>Выбранная песня: {video?.song?.author_name + ' | ' + video?.song?.title}</p>
-            {renderVideo}
-            <button onClick={() => acceptVideo(video.id)} style={{background: 'green', color: 'white'}}>Принять</button>
-            <button onClick={() => declineVideo(video.id)} style={{background: 'crimson', color: 'white'}}>Отклонить
-            </button>
-        </article>
-    )
+  return (
+    <article
+      style={{
+        border: "solid #0647C7",
+        margin: "10px auto",
+        width: "300px",
+        padding: "10px 15px",
+        borderRadius: "20px",
+      }}
+    >
+      <img
+        src={video?.users?.image}
+        style={{ width: "100%" }}
+        alt="Нету фото"
+      />
+      <p>Имя: {video?.users?.name + " " + video?.users?.name}</p>
+      <p>Возраст: {video?.users?.age}</p>
+      <p>Номер телефона: {video?.users?.phone_number}</p>
+      <p>Адрес электронной почты: {video?.users?.email}</p>
+      <p>Город: {video?.users?.city}</p>
+      <p>Ссылка на соцсеть: {video?.users?.social_media_link}</p>
+      <p>
+        Выбранная песня: {video?.song?.author_name + " | " + video?.song?.title}
+      </p>
+      {renderVideo}
+      <button
+        onClick={() => acceptVideo(video.id)}
+        style={{
+          color: "white",
+          backgroundColor: "#06C92E",
+          padding: "8px 25px",
+          borderRadius: "10px",
+          fontSize: "20px",
+          margin: "10px 10px 0 0",
+        }}
+      >
+        Принять
+      </button>
+      <button
+        onClick={() => declineVideo(video.id)}
+        style={{
+          background: "crimson",
+          color: "white",
+          padding: "8px 13px",
+          borderRadius: "10px",
+          fontSize: "20px",
+        }}
+      >
+        Отклонить
+      </button>
+    </article>
+  );
 }
--- src/pages/Moderation/VideoModeration.js
+++ src/pages/Moderation/VideoModeration.js
@@ -1,65 +1,87 @@
-import React, {useCallback, useEffect, useMemo, useState} from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import useAxiosPrivate from "../../hooks/useAxiosPrivate.js";
 import debounce from "lodash/debounce.js";
 import Plyr from "plyr-react";
 import Video from "../../components/Moderation/Video.js";
 
 export default function VideoModeration() {
-    const privateAxios = useAxiosPrivate()
-    const [videos, setVideos] = useState([])
-    const [noMoreVideosFound, setNoMoreVideosFound] = useState(false)
-    const [skipVideosCount, setSkipVideosCount] = useState(0)
-    const [password, setPassword] = useState('')
-    const [accessApproved, setAccessApproved] = useState(false)
+  const privateAxios = useAxiosPrivate();
+  const [videos, setVideos] = useState([]);
+  const [noMoreVideosFound, setNoMoreVideosFound] = useState(false);
+  const [skipVideosCount, setSkipVideosCount] = useState(0);
+  const [password, setPassword] = useState("");
+  const [accessApproved, setAccessApproved] = useState(false);
 
-    useEffect(() => {
-        if (accessApproved) {
-            findMore()
+  useEffect(() => {
+    if (accessApproved) {
+      findMore();
+    }
+  }, [accessApproved]);
+
+  const findMore = useCallback(
+    debounce(async () => {
+      let response = {};
+      try {
+        response = await privateAxios.get(
+          "content/videos-to-moderate?skip=" + skipVideosCount
+        );
+
+        const allVideos = [...videos, ...response.data];
+        setVideos(allVideos);
+        setSkipVideosCount(allVideos.length);
+
+        if (response.data.length == 0) {
+          setNoMoreVideosFound(true);
+          return;
         }
-    }, [accessApproved])
+      } catch (err) {
+        console.log(err);
+      }
+    }, 1000),
+    [privateAxios, skipVideosCount]
+  );
 
-    const findMore = useCallback(
-        debounce(async () => {
-            let response = {}
-            try {
-                response = await privateAxios.get('content/videos-to-moderate?skip=' + skipVideosCount)
+  return (
+    <>
+      {accessApproved ? (
+        <div className="moderation">
+          {videos.map((video) => {
+            return <Video video={video} />;
+          })}
 
-                const allVideos = [...videos, ...response.data]
-                setVideos(allVideos)
-                setSkipVideosCount(allVideos.length)
-
-                if (response.data.length == 0) {
-                    setNoMoreVideosFound(true)
-                    return
-                }
-            } catch (err) {
-                console.log(err)
-            }
-        }, 1000),
-        [privateAxios, skipVideosCount]
-    )
-
-    return(
-        <>
-            {accessApproved ? <div className='moderation'>
-                    {videos.map(video => {
-                        return (
-                            <Video video={video}/>
-                        )
-                    })}
-
-                    <button style={{border: 'solid', borderRadius: '10px'}} onClick={findMore}>Загрузить ещё видео</button>
-                </div> :
-                <div>
-                    <label>Пароль</label>
-                    <input style={{border: 'solid black'}} type='password' value={password} onChange={(e) => setPassword(e.target.value)}/>
-                    <button onClick={() => {
-                        if (password == process.env.REACT_APP_SECRET_PASSWORD) {
-                            setAccessApproved(true)
-                        }
-                    }}>Войти
-                    </button>
-                </div>}
-        </>
-    )
+          <button
+            style={{
+              border: "2px solid blueviolet",
+              borderRadius: "10px",
+              display: "block",
+              margin: "10px auto",
+              fontSize: "28px",
+            }}
+            onClick={findMore}
+          >
+            Загрузить ещё видео
+          </button>
+        </div>
+      ) : (
+        <div>
+          <label>Пароль</label>
+          <input
+            style={{ border: "solid black" }}
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            onClick={() => {
+              if (password == process.env.REACT_APP_SECRET_PASSWORD) {
+                setAccessApproved(true);
+              }
+            }}
+          >
+            Войти
+          </button>
+        </div>
+      )}
+    </>
+  );
 }
